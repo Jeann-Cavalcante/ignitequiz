149b59c5d0c13b19d436098b862a1521edba3e79
= <jean.cavalcante@helppi.com.br>
2024-06-15 17:03:42 -0300

    first commit

---
+++ src/components/Level/index.tsx
@@ -0,0 +1,92 @@
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  View,
+  Pressable,
+} from "react-native";
+
+import { THEME } from "../../styles/theme";
+import { styles } from "./styles";
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { useEffect } from "react";
+
+const TYPE_COLORS = {
+  EASY: THEME.COLORS.BRAND_LIGHT,
+  HARD: THEME.COLORS.DANGER_LIGHT,
+  MEDIUM: THEME.COLORS.WARNING_LIGHT,
+};
+
+type Props = TouchableOpacityProps & {
+  title: string;
+  isChecked?: boolean;
+  type?: keyof typeof TYPE_COLORS;
+};
+
+export function Level({
+  title,
+  type = "EASY",
+  isChecked = false,
+  ...rest
+}: Props) {
+  const scale = useSharedValue(1);
+  const checked = useSharedValue(1);
+
+  const COLOR = TYPE_COLORS[type];
+
+  const animatedContentStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+      backgroundColor: interpolateColor(
+        checked.value,
+        [0, 1],
+        ["transparent", COLOR]
+      ),
+    };
+  });
+
+  const animatedTextStyles = useAnimatedStyle(() => {
+    return {
+      color: interpolateColor(
+        checked.value,
+        [0, 1],
+        [COLOR, THEME.COLORS.GREY_100]
+      ),
+    };
+  });
+
+  function handlePressIn() {
+    scale.value = withTiming(0.95);
+  }
+
+  function handlePressOut() {
+    scale.value = withTiming(1);
+  }
+
+  useEffect(() => {
+    checked.value = withTiming(isChecked ? 1 : 0);
+  }, [isChecked]);
+
+  return (
+    <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut} {...rest}>
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            borderColor: COLOR,
+          },
+          animatedContentStyles,
+        ]}
+      >
+        <Animated.Text style={[styles.title, animatedTextStyles]}>
+          {title}
+        </Animated.Text>
+      </Animated.View>
+    </Pressable>
+  );
+}
